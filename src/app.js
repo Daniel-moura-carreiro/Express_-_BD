ed0622e72041ffa38a6ad34a2d00503c8d8ca784
Daniel-moura-carreiro <mouradaniel0818@gmail.com>
2026-08-25 20:25:46 -0300

    Revert "aula de sexta"

--- src/app.js
+++ src/app.js
@@ -1,13 +1,61 @@
-import express from 'express'
-
+import express from "express"
 const app = express()
 
-app.use(express.json())
+// Express dava interpretar o corpo (body) com Json
+app.use(express.json)
 
+// Mock
+const alunos = [
+  {id: 1, nome:'Bruno', curso: 'ADS'},
+  {id: 2, nome:'Maria', curso: 'ADS'},
+]
+
+// função auxiliar
+function buscarIndexAluno(id) {
+  return alunos.filter( aluno => aluno.id == id)
+}
+
+// função auxiliar
+function buscarAlunoPorId(id) {
+  return alunos.findIndex( aluno => aluno.id == id)
+}
+
+//criando a rota raiz
 app.get('/', (req, res) => {
-  res.status(200).json({
-    mensagem: 'API REST funcionando'
-  })
+  res.send('Minha API REST com Express!')
+})
+
+// Rota lista alunos GET
+app.get('/alunos', (req, res) => {
+    res.status(200).send(alunos);
+})
+
+// Rota lista alunos POST
+app.post('/alunos', (req, res) => {
+    alunos.push(req.body)
+    res.status(201).send('Aluno cadastrado com sucesso!');
+})
+
+// Buscar aluno por id
+app.get('/aluno/:id', (req, res) => {
+  let index = buscarIndexAluno(req.params.id)
+  res.json.buscarAlunoPorId(req.params.id)
+})
+
+// Delete
+app.delete('/alunos/: id', (req, res) => {
+    let index = buscarIndexAluno(req.params.id)
+   // console.log(index)
+   alunos.splice(index, 1)
+   res.send('Aluno co id ${req.params.id} excluindo com sucesso!')
+})
+
+// Atualizar aluno
+app.put('/aluno/:id', (req, res) => {
+  let index = buscarIndexAluno(req.params.id)
+  aluno[index].nome = req.body.nome
+  aluno[index].curso = req.body.curso
+  res.send(alunos)
 })
 
 export default app
